4fe13a2d4f2c2da2af5789375805b1a98dd2e7c4
Luuk Diederik <flipmil3@hotmail.com>
2017-08-25 22:34:39 +0200

    m,n

---
+++ Farming Game/Assets/Scripts/Game/Item.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType {MANA, HEALTH};
+
+public class Item : MonoBehaviour {
+
+    public ItemType type;
+    public Sprite spriteNeutral;
+    public Sprite spriteHighlighted;
+    public int maxSize;
+
+
+    public void Use()
+    {
+        switch (type)
+        {
+            case ItemType.MANA:
+                Debug.Log("Mana potion used");
+                break;
+            case ItemType.HEALTH:
+                Debug.Log("Health potion used");
+                break;
+        }
+    }
+}
---
+++ Farming Game/Assets/Scripts/Game/Player.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ExtMethods {
+    public static Vector3 Clamp( this Vector3 v, float min, float max ) {
+        v.x = Mathf.Clamp( v.x, min, max );
+        v.y = Mathf.Clamp( v.y, min, max );
+        v.z = Mathf.Clamp( v.z, min, max );
+        return v;
+    }
+    public static Vector3 Clamp( this Vector3 v, Vector3 min, Vector3 max ) {
+        v.x = Mathf.Clamp( v.x, min.x, max.x );
+        v.y = Mathf.Clamp( v.y, min.y, max.y );
+        v.z = Mathf.Clamp( v.z, min.z, max.z );
+        return v;
+    }
+}
+
+public class Player : MonoBehaviour {
+
+    public float Friction = 0.35f;
+    public float Acceleration = 0.5f;
+    public float MaxVelocity = 3f;
+    public Vector3 Velocity;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if ( Input.GetKey( KeyCode.W ) ) Velocity.y += Acceleration;
+        if ( Input.GetKey( KeyCode.S ) ) Velocity.y -= Acceleration;
+        if ( Input.GetKey( KeyCode.D ) ) Velocity.x += Acceleration;
+        if ( Input.GetKey( KeyCode.A ) ) Velocity.x -= Acceleration;
+
+        if ( !Input.GetKey( KeyCode.W ) && !Input.GetKey( KeyCode.S ) ) Velocity.y = Mathf.Lerp( Velocity.y, 0, Friction );
+        if ( !Input.GetKey( KeyCode.A ) && !Input.GetKey( KeyCode.D ) ) Velocity.x = Mathf.Lerp( Velocity.x, 0, Friction );
+
+        if ( Velocity.x <= -MaxVelocity * 0.75 && Velocity.y <= -MaxVelocity * 0.75
+             || Velocity.x <= -MaxVelocity * 0.75 && Velocity.y >= MaxVelocity * 0.75
+             || Velocity.x >= MaxVelocity * 0.75 && Velocity.y <= -MaxVelocity * 0.75
+             || Velocity.x >= MaxVelocity * 0.75 && Velocity.y >= MaxVelocity * 0.75 )
+            Velocity = Velocity.Clamp( -MaxVelocity * 0.75f, MaxVelocity * 0.75f );
+        else Velocity = Velocity.Clamp( -MaxVelocity, MaxVelocity );
+
+        transform.position += Velocity * Time.deltaTime;
+    }
+}
